a1dee1e0cf7b66962d3340c6891ce8f383ac86e0
shayr <shayrozner@gmail.com>
2021-08-30 19:17:43 +0300

    my first file

--- odd-even.py
+++ odd-even.py
@@ -1,29 +1,33 @@
 the_box = {1: {},
-           2: {}}
+        2: {}}
 # dic1 add even/odd, and dict2 the number inserted
 def picka():
     a = input("Odd or Even: ")
     # print("You've picked: " + a)
     the_box.update({1:a})
 picka()
-def chu():
+def rand():
     import random
     r = random.randint(1, 100)
-    print(r)
-chu()
-# def even_odd():
-#     num1 = input("pick a number:")
-#     num2 = input("pick a number:")
-#     a = int(num1) + int(num2)
-#     if (a % 2) == 0:
-#         the_box.update({2:a})
-#         print("{0} is even".format(a))
-#     else:
-#         the_box.update({2:a})
-#         print("{0} is Odd".format(a))
-# even_odd()
+    return r
+x = rand()
 def game_on():
-    num1 = input("pick a number: ")
-    num2 =
-game_on()
+    num1 = int(input("pick a number: "))
+    num2 = x
+    return num1 + num2
+s = game_on()
+def even_odd():
+    if (s % 2) == 0:
+        the_box.update({2:s})
+        print("{0} is even".format(s))
+    else:
+        the_box.update({2:s})
+        print("{0} is Odd".format(s))
+even_odd()
+def finals():
+    if {1:s} == {2:s}:
+        print("You Won")
+    else:
+        print("You Lose")
+finals()
 print(the_box)
