bedb7d3a18ac13721270ab740ef2d1bf02e3b225
shayr <shayrozner@gmail.com>
2021-09-01 21:51:40 +0300

    my first file

--- odd-even.py
+++ odd-even.py
@@ -5,32 +5,31 @@ while i > 0:
     # dic1 add even/odd, and dict2 the number inserted
     def picka():
         a = input("odd or even: ")
+        the_box[1]= a
         # print("You've picked: " + a)
-        the_box.update({1: a})
     picka()
     def rand():
         import random
-        r = random.randint(1, 100)
-        return r
-    x = rand()
+        return random.randint(1, 100)
+    rand()
     def game_on():
         num1 = int(input("pick a number: "))
-        num2 = x
+        num2 = rand()
         return num1 + num2
     s = game_on()
     def even_odd():
+        the_box[2]= s
         if (s % 2) == 0:
-            the_box.update({2: s})
             print("{0} is even".format(s))
+            the_box[3]= "even"
         else:
-            the_box.update({2: s})
             print("{0} is odd".format(s))
+            the_box[3] = "odd"
     even_odd()
     def finals():
-        if ({1: s} == {2: s}):
+        if the_box[1] == the_box[3]:
             print("You Won")
         else:
             print("You Lose")
     finals()
     print(the_box)
-    # odd == odd => says i lost, y?
