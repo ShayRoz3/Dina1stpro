39aa93f89efb9a596f6cddd550094e60d0c345fd
shay rozner <shayrozner@gmail.com>
2022-06-06 19:28:00 +0300

    try

--- FileItUp.py
+++ FileItUp.py
@@ -1,1 +1,17 @@
-print("hello world")
+import csv
+# print(file.read())
+def flower_dict():
+        file = open("iris.data.csv", "r")
+        flower_dictionary = {}
+        cvs_f = list(csv.reader(file))
+        print(cvs_f)
+        # ran_ge = (cvs_f)
+        # for row in cvs_f:
+                # print(row)
+                # for col in row:
+                #         print(col[0:3])
+                        # flower_dictionary.update(keys = cvs_f[:-1][-1] )
+                        # print(flower_dictionary)
+                        # print(col)
+
+flower_dict()
---
+++ TRYOUTS.py
@@ -0,0 +1,58 @@
+# int(input("whats  your name"))
+
+#
+# name =  input("what is your name:")
+# # surname = input("what is your last name")
+# # id = print(name + " "+ surname)
+#
+# NUM1 = 5.7
+# RANDI = 8
+# NUM3 = print(NUM1+RANDI)
+
+# listy = ["price", "compose", "test"]
+# touply = tuple(listy)
+#
+# print(listy)
+# print(touply)
+#
+# listy.append("wow")
+# print(listy)
+# print(touply)
+
+#list1 = ["history", "math", "physics"]
+# tuple1 = tuple(list1)
+# print(list1)
+# print(tuple1)
+# list1[0] = "art"
+#
+# print(list1)
+# print(tuple1)
+#
+# x = 1
+# y = x
+# print(x, y)
+# x = 2
+# print(x, y)
+# y = 5
+# x = 1
+# print(y, x)
+#
+#
+# def ab(a,b):
+#     return(a,b)
+#     def inab(a,b):
+#         return (a+b)
+#     new = inab(a,b)
+#     print(new+5)
+#
+# ab(3,2)
+
+def tra_bud():
+    name = input("Hello, what is your name? " )
+    return name
+def age():
+    age = input("how old are you")
+    print(age, name)
+
+tra_bud()
+age()
